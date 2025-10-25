931968a86ffdd80ae549ebf564aa80a786304a8d
Devang <devang2000singh@gmail.com>
2025-10-25 23:17:07 +0530

    Added daisy UI

---
+++ app/navbar.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import React, { useState } from "react";
+
+interface IChat {
+  id: string;
+  name: string;
+}
+
+const Navbar = ({ children }: { children: React.ReactNode }) => {
+  const [chats, setChats] = useState<IChat[]>([{ id: "1", name: "My Chat" }]);
+
+  return (
+    <div className="drawer drawer-open">
+      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+
+      <div className="drawer-content">{children}</div>
+
+      <div className="drawer-side is-drawer-close:overflow-visible">
+        <label
+          htmlFor="my-drawer-4"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <div className="is-drawer-close:w-14 is-drawer-open:w-64 bg-base-200 flex flex-col items-start min-h-full">
+          <ul className="menu w-full grow">
+            <li>
+              <button
+                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                data-tip="New Chat"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+
+                <span className="is-drawer-close:hidden">New Chat</span>
+              </button>
+            </li>
+
+            {chats.map((chat) => (
+              <li key={chat.id}>
+                <button
+                  className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                  data-tip={chat.name}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-4 inline-block my-1.5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+
+                  <span className="is-drawer-close:hidden">{chat.name}</span>
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <div
+            className="m-2 is-drawer-close:tooltip is-drawer-close:tooltip-right"
+            data-tip="Open"
+          >
+            <label
+              htmlFor="my-drawer-4"
+              className="btn btn-ghost btn-circle drawer-button is-drawer-open:rotate-y-180"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2"
+                fill="none"
+                stroke="currentColor"
+                className="inline-block size-4 my-1.5"
+              >
+                <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+                <path d="M9 4v16"></path>
+                <path d="M14 10l2 2l-2 2"></path>
+              </svg>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
